4983df2ea6e2069cc93f910c88f3b7c3c991ed98
岳克奎 <738816656@qq.com>
2017-07-12 16:10:44 +0800

    创建美颜相机
    创建美颜相机

---
+++ STCamera/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  STCamera
+//
+//  Created by 岳克奎 on 2017/7/12.
+//  Copyright © 2017年 ST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
